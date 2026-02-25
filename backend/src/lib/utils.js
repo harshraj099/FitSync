20b07c039dd6c7ab1f6e51124ae8cfcd605ef6aa
harshraj099 <albatross.phoenix99@gmaail.com>
2026-02-25 23:29:08 +0530

    cors error fixed

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -7,19 +7,20 @@ export const generateToken = (userId, res) => {
 
      const isProduction = process.env.NODE_ENV === "production";
 
-  res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-    httpOnly: true,
-    secure: isProduction,            // HTTPS only in production
-    sameSite: isProduction ? "none" : "lax", // CRITICAL FIX
-  });
+  // res.cookie("jwt", token, {
+  //   maxAge: 7 * 24 * 60 * 60 * 1000,
+  //   httpOnly: true,
+  //   secure: isProduction,            // HTTPS only in production
+  //   sameSite: isProduction ? "none" : "lax", // CRITICAL FIX
+  // });
 
-    // res.cookie("jwt", token, {
-    //     maxAge: 7 * 24 * 60 * 60 * 1000,  // 7 days
-    //     httpOnly: true,  // Makes the cookie accessible only by the server
-    //     secure: process.env.NODE_ENV !== "development",  // In development, cookies are not sent over https
-    //     sameSite:"None",
-    // });
+  res.cookie("jwt", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  path: "/",
+});
 
     return token;
 };
